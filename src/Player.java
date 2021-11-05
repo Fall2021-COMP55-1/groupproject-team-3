f100ca7074f90f973c7edd171e6ee8d08e1e761a
Ty <t_hendricks1@u.pacific.edu>
2021-11-05 11:35:48 -0700

    Created temp Player class (will be changed later)
    Implemented Player into NewGamePane

---
+++ src/Player.java
@@ -0,0 +1,36 @@
+import java.awt.Image;
+
+import acm.graphics.GImage;
+import acm.graphics.GRect;
+
+public class Player {
+	private int x, y, width = 32, height = 32;
+	private GImage sprite = new GImage("res/player/PCU1.png");
+	private boolean visible;
+	
+	
+	public Player(int x, int y)   {
+		this.x = x;
+		this.y = y;
+		visible = true;
+	}
+	
+	public int getX()   {return x;}
+	
+	public int getY()   {return y;}
+	
+	public GImage getImage()   {return sprite;}
+	
+	public boolean isVisible()   {return visible;}
+	
+	public void setVisible(boolean visible)   {this.visible = visible;}
+	
+	public GRect getBounds()   {return new GRect(x, y, width, height);}
+	
+	protected void loadImage(Image image)   {sprite.setImage(image);}
+	
+	protected void move(int x, int y)   {
+		sprite.move(x, y);
+	}
+	
+}
