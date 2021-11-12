c4b967b77b9c89e8d714269aa989e2910ac4f2c9
kevinnguyen <k_nguyen159@u.pacific.edu>
2021-11-12 11:48:23 -0800

    Added the load game UI to make it work. Still working on the save parts.

--- src/OptionPane.java
+++ src/OptionPane.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 
 
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/SavePane.java
+++ src/SavePane.java
@@ -1,41 +1,78 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
 import acm.graphics.GObject;
 
+
 public class SavePane extends GraphicsPane {
 	// you will use program to get access to all of the GraphicsProgram calls
 	private MainApplication program; 
-
+	
 	private GImage img;
-	private GParagraph para;
+	private GButton loadGame; 
+	private GButton Back; 
+	private GButton save1; 
+	private GButton save2;
+	private GButton save3; 
+	private final int WIDTH = 200; 
+	private final int HEIGHT = 88; 
 
 	public SavePane(MainApplication app) {
+		super();
 		this.program = app;
-		img = new GImage("robot head.jpg", 100, 100);
-		para = new GParagraph("this is save pane where there are three saved files are ", 150, 300);
-		para.setFont("Arial-24");
+		program = app;
+		double X = app.getWidth()/2 - WIDTH/2 -7;
+		img = new GImage("res/texture/load Game.png", 0, 0);
+		img.setSize(800, 640);	
+		loadGame = new GButton("", X, 418, WIDTH, HEIGHT);
+		Back = new GButton("", X, 532, WIDTH, HEIGHT);
+		//save1 = new GButton("", X, );
+		//save2 = new GButton("", X, 418, WIDTH, HEIGHT); 
+		//save3 = new GButton("", );
+		
 	}
 
 	@Override
 	public void showContents() {
 		program.add(img);
-		program.add(para);
+		program.add(loadGame);
+		program.add(Back);
+		//program.add(save1);
+		//program.add(save2);
+		//program.add(save3);
+		
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(img);
-		program.remove(para);
+		program.remove(loadGame);
+		program.remove(Back);
+		//program.remove(save1);
+		//program.remove(save2);
+		//program.remove(save3);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		para.setText("you need\nto click\non the eyes\nto go back");
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == img) {
+		if (obj == Back) {
 			program.switchToMenu();
 		}
+		if (obj == save1) {
+			program.switchToNewGame();
+		}
+		if (obj ==  save2) {
+			program.switchToNewGame();
+		}
+		if (obj == save3) {
+			program.switchToNewGame();
+		}
 	}
+
 }
 
