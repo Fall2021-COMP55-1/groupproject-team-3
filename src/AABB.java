8a82bfbdf91b1004147d91fa37e9357d26230201
Sloan Kim <g_kim22@u.pacific.edu>
2021-11-15 11:12:45 -0800

    Updated AABB class

--- src/AABB.java
+++ src/AABB.java
@@ -1,33 +1,52 @@
-
 public class AABB {
-	public double minX, minY, maxX, maxY;
-	public int width, height;
+	private int height;
+	private int width;
+	private double minX, minY, maxX, maxY;
 	
-	AABB(double minX, double minY, int width, int height){
-		this.minX = minX; 
-		this.minY = minY;
-		this.width = width;
+	//Not really sure about this variable
+	
+	public AABB(double x, double y, int width, int height) {
+		minX = x;
+		minY = y;
 		this.height = height;
-		maxX = minX+width;
-		maxY = minY+height;
+		this.width = width;
+		maxX = x + width;
+		maxY = y + height;
 		
 	}
 	
-	public boolean Collision(AABB box) {
-		if(box.minX > maxX || minX > box.maxX) {
-			return false;
-		}
-		if(box.minY > maxY || minY > box.maxY) {
-			return false;
-		}
-		return true;
-	}
 	
-	public void update(double X, double Y) {
-		minX+=X;
-		minY+=Y;
-		maxX+=X;
-		maxY+=Y;
+	public boolean intersectAABB( AABB box) {
+			if( box.getX() > maxX || minX > box.getMaxX()) return false;
+			if( box.getY()> maxY || minY > box.getMaxY() ) return false;
+			return true;
+			}
+	
+	
+	
+	public boolean intersectCircleAABB(double x, double y, float r) {
+		float d = 0.0f;
+		if( x < minX ) d += (x - minX)*(x - minX);
+		if( x > maxX ) d += (x - maxX)*(x - maxX);
+		if( y < minY ) d += (y - minY)*(y - minY);
+		if( y > maxY ) d += (y - maxY)*(y - maxY);
+		return d < r*r;
+	}
+	public double getY() {
+		return minY;
+		
+	}
+	public double getX() {
+		return minX;
+		
+	}
+	public double getMaxY() {
+		return maxY;
+		
+	}
+	public double getMaxX() {
+		return maxY;
+		
 	}
 	
 }
