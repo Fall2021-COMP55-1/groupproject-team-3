b4d0a6087f413d80ae8cb0621fb673b0f77a74a7
Sloan Kim <g_kim22@u.pacific.edu>
2021-12-03 19:30:54 -0800

    Interactions with npc does not work

--- src/GamePanes/BedRoomGamePane.java
+++ src/GamePanes/BedRoomGamePane.java
@@ -25,57 +25,6 @@ public class BedRoomGamePane extends GraphicsPane implements ActionListener {
 		setWalls();
 	}
 
-<<<<<<< HEAD
-	public void setWalls() {
-		GRect wall1 = new GRect(0,0,800,96);
-		GRect wall2 = new GRect(0,256,800,96);
-		GRect wall3 = new GRect(0,448, 704 ,192);
-		GRect wall4 = new GRect(0,0,32,640);
-		GRect wall5 = new GRect(768,0,32,640);
-		GRect wall6 = new GRect(352,32,64,224);
-		GRect wall7 = new GRect(704,512,64,128);
-		
-		walls.add(wall1); walls.add(wall2); walls.add(wall3); walls.add(wall4); walls.add(wall5); walls.add(wall6); walls.add(wall7);
-		
-		for (int i = 0; i < 7; ++i)   {walls.get(i).setFilled(true);}
-		
-		GRect npc = new GRect(102, 152, 10, 10);
-		npc.setVisible(false);
-		walls.add(npc);
-	}
-	
-	public void setGUI() {
-		pauseImge = new GImage("res/texture/pause.png", 768, 0); 
-		pauseImge.setSize(32, 32);
-		pauseImge.setVisible(true);
-		pauseButton = new GButton("", 768, 0, 32, 32); 
-		choice1 = new GImage("res/interactive choices/Choice 1.png", 500, 555);
-		choice2 = new GImage("res/interactive choices/Choice 2.png", 500, 600); 
-		choice1.setSize(150, 40);
-		choice2.setSize(150, 40); 
-		killHim = new GButton("", 500, 555, 150, 40);
-		spareHim = new GButton("", 500, 600, 150, 40);
-	}
-	
-	public void setItems() {
-		Item winningKey = new Item("Key",new GImage("res/inventory/Small Key.png"), ItemType.KEY, "bedR");
-		winningKey.setX(422);
-		winningKey.setY(217);
-		winningKey.setRoomType(RoomType.OUT);
-		winningKey.setDescription("Key of the house");
-		program.addItem(winningKey);
-		Item itemKey2 = new Item("Key", new GImage("res/inventory/Small Key.png"), ItemType.KEY, "bedR");
-		itemKey2.setX(116);
-		itemKey2.setY(184);
-		itemKey2.setRoomType(RoomType.BEDROOMR);
-		itemKey2.setDescription("Key to master bedroom");
-		program.addItem(itemKey2);
-	}
-	
-	
-
-=======
->>>>>>> branch 'main' of https://github.com/Fall2021-COMP55-1/groupproject-team-3.git
 	@Override
 	public void showContents() {
 		//1. walls
@@ -87,22 +36,12 @@ public class BedRoomGamePane extends GraphicsPane implements ActionListener {
 		//4. player and monster and npc
 		program.addPlayer(playerX, playerY);
 		program.player.getInventory();
-<<<<<<< HEAD
-		if(program.NPC.isDead() == false)   {program.addNPC(100, 150);}
-		if(program.NPC.isDead())   {walls.remove(walls.size() - 1);}
-		program.addMonster(playerX + 16,  playerY + 50);
-		program.monsterTimer.setInitialDelay(3000);
-		program.monsterTimer.start();
-		//inventory hot bar image
-		//4. inventory hot bar image
-=======
 		program.addMonster(playerX + 20,  playerY + 50);
 		program.monsterTimer.setInitialDelay(3000);
 		program.monsterTimer.start();
-		if(!program.NPC.isDead()) {program.addNPC(540, 450);
+		if(!program.NPC.isDead()) {program.addNPC(100, 150);
 		}else {walls.remove(walls.size() - 1);}
 		//5. inventory hot bar image
->>>>>>> branch 'main' of https://github.com/Fall2021-COMP55-1/groupproject-team-3.git
 		program.add(Inventory.INVENTORY_IMG, Inventory.INVENTORY_X, Inventory.INVENTORY_Y);
 		//6. items on the map
 		program.Items(true, MapType.BEDR);
@@ -110,31 +49,18 @@ public class BedRoomGamePane extends GraphicsPane implements ActionListener {
 		for (int i=0; i<program.player.getInventory().numInvItems(); i++) {
 			program.add(program.player.getInventory().itemAt(i).getInvSprite());
 		}
-<<<<<<< HEAD
-		//doors
-		program.addDoorBedRoom();
-		program.player.getInventory().setHighlightVisible(true);
-		//7. interactive choices
-		
-=======
 		//8. interactive choices
->>>>>>> branch 'main' of https://github.com/Fall2021-COMP55-1/groupproject-team-3.git
 		choice1 = new GImage("res/interactive choices/Choice 1.png", 500, 555);
 		choice2 = new GImage("res/interactive choices/Choice 2.png", 500, 600); 
 		choice1.setSize(150, 40);
 		choice2.setSize(150, 40); 
 		killHim = new GButton("", 500, 555, 150, 40);
 		spareHim = new GButton("", 500, 600, 150, 40);
-<<<<<<< HEAD
-		
-		//8. GUI
-=======
 		program.add(choice1);
 		program.add(choice2);
 		program.add(killHim);
 		program.add(spareHim);
 		//9. GUI
->>>>>>> branch 'main' of https://github.com/Fall2021-COMP55-1/groupproject-team-3.git
 		program.addGUI();
 	}
 	
@@ -173,18 +99,6 @@ public class BedRoomGamePane extends GraphicsPane implements ActionListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-<<<<<<< HEAD
-		if (obj == program.pauseButton) {
-			program.monsterTimer.stop();
-			program.pause();
-		}
-		if (obj == program.resume) {
-			program.resume();
-			program.monsterTimer.setInitialDelay(0);
-			program.monsterTimer.restart();
-		}	
-		
-		if (obj == program.quit) {System.exit(0);}
 		
 		if (obj == killHim || obj == spareHim)   {
 			if (obj == killHim)   {program.NPC.setDead(true);}
@@ -193,9 +107,8 @@ public class BedRoomGamePane extends GraphicsPane implements ActionListener {
 			program.paused = false;
 			program.monsterTimer.start();
 		}
-=======
+
 		program.pauseMenu(obj);
->>>>>>> branch 'main' of https://github.com/Fall2021-COMP55-1/groupproject-team-3.git
 	
 		//to select item on the inventory hot bar
 		Inventory playerInv = program.player.getInventory();
